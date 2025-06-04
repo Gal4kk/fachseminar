4f880d6c5f24faa94731057692f2874736f797fe
92Serdar <132610561+92Serdar@users.noreply.github.com>
2025-06-04 15:35:48 +0200

    Add files via upload
    Pokemonfabrik in Greenfoot

---
+++ Pokemonfabrik/Pokemonfabrik.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot und MouseInfo)
+
+/**
+ * Ergänzen Sie hier eine Beschreibung für die Klasse MyWorld.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Pokemonfabrik extends World
+{
+
+    /**
+     * Konstruktor für Objekte der Klasse MyWorld
+     * 
+     */
+    public Pokemonfabrik()
+    {    
+        // Erstellt eine neue Welt mit 600x400 Zellen und einer Zell-Größe von 1x1 Pixeln. 
+        super(600, 400, 1); 
+        
+        setBackground("pokefabrik.png");
+    }
+}
